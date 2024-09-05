d582815c355fc37cc1bf7cba490f39534be6597f
cspeisman <cspeisman@gmail.com>
2024-09-05 19:56:14 -0400

    uses bun-promptx instead of cli-kit due to bun bug

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 import {teardown} from "./src/cmds/teardown.ts";
 import {setup} from "./src/cmds/setup.ts";
 import {getArgValue} from "./src/utils/getArgumentValue.ts";
-import {prompt} from "@astrojs/cli-kit";
+import {createSelection} from "bun-promptx";
 
 const hasDestroyFlag = Bun.argv.includes("--destroy");
 const hasHelpFlag = Bun.argv.includes("-h") || Bun.argv.includes("--h");
@@ -15,19 +15,18 @@ if (hasDestroyFlag) {
 
   let framework = getArgValue(Bun.argv, '--framework')
   if (!framework) {
-     const {framework: selectedFramework} = await prompt({
-      name: 'framework',
-      type: 'select',
-      label: '',
-      message: 'Which framework would you like to use?',
-      initial: 'astro',
-      choices: [
-        {value: 'astro', label: 'Astro'},
-        {value: 'remix', label: 'Remix'},
-        {value: 'nextjs', label: 'Nextjs'},
-      ],
-    })
-     framework = selectedFramework;
+    let frameworks = [
+      {text: 'astro'},
+      {text: 'remix' },
+      {text: 'nextjs'},
+    ];
+    const {selectedIndex} = createSelection(frameworks, {
+       headerText: "Select your framework"
+     })
+
+    if (selectedIndex) {
+     framework = frameworks[selectedIndex].text;
+    }
   }
   await setup(Bun.argv[2], framework);
 }
